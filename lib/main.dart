5c84c4d4ebc14848be9a5dcc7d594e3bff71677e
evgesha1247 <dnsbtrq25@gmail.com>
2022-10-23 20:46:49 +0300

    ---

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'ui/screens_factory.dart/widget_factory.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'firebase_options.dart';
 
 
