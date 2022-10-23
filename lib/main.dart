aa513f7dcc203c2e4e033e6023c3dce20d4cf322
evgesha1247 <dnsbtrq25@gmail.com>
2022-10-23 20:55:40 +0300

    проверка на какой платформе запускается

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:text/firebase_options.dart';
 import 'ui/screens_factory.dart/widget_factory.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 
-Future main() async {
+
+Future<void> main() async {
   final factor = ScreensFactory();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  try {
+    await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  } catch (e) {
+    print(
+        '$e - крч , не можешь подключить облочные платформы на этом устройстве');
+  }
   await Hive.initFlutter();
   runApp(factor.makeMainApp());
 }
